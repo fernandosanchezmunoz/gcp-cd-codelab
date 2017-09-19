d8d4e49128465f99afbd4d4a94b39ad8e781192d
Fernando Sanchez <fersanchez@fersanchez2.hot.corp.google.com>
2017-09-19 17:11:15 -0400

    correct main.go curly-parenth

--- main.go
+++ main.go
@@ -1,15 +1,15 @@
 package main
 
-import {
-	"io"
-	"net/http"
-}
+import (
+    "io"
+    "net/http"
+)
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello World!")
+    io.WriteString(w, "Hello World!")
 }
 
 func main() {
-	http.HandleFunc("/", hello)
-	http.ListenAndServe(":80", nil)
+    http.HandleFunc("/", hello)
+    http.ListenAndServe(":80", nil)
 }
