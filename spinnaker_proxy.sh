9c65c7a38db3c991b1aed7d14fd7dcc479df8db4
Fernando Sanchez <fernandosanchezmunoz@gmail.com>
2017-09-19 17:40:44 -0400

    spinnaker_proxy.sh

--- spinnaker_proxy.sh
+++ spinnaker_proxy.sh
@@ -2,7 +2,7 @@ GCP_PROJECT_ID=fersanchez-k8s-demo
 GCP_ZONE=us-east1-c
 SPINNAKER_INSTANCE=fersanchez-spinnaker-codelab
 
-gcloud compute ssh $SPINNAKER_INSTANC \
+gcloud compute ssh $SPINNAKER_INSTANCE \
     --project $GCP_PROJECT_ID \
     --zone $GCP_ZONE \
     --ssh-flag="-L 8084:localhost:8084" \
